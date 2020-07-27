c1c39b017d8bdba090a907d9714ac3ad778b130b
damienlevitt <damien.levitt@sjsu.edu>
2020-07-26 23:34:42 -0700

    Cleaned up the commenting and added instrucitons.

--- Bot.py
+++ Bot.py
@@ -1,7 +1,18 @@
-# This script requires you find the product item number of the desired item.
-# This can be done by inspecting the item in chrome and replacing the 5 digit number with that of your desired product.
-# The URL must be changed to the location of the desired product page.
-# Make sure to fill in all the REQUIRED information below.
+# Make sure to read the INSTRUCTIONS and fill in all the REQUIRED information below. I recommend taking a look at
+# the README to ensure you have the correct libraries and necessary tools downloaded.
+
+# After testing, it may be faster to have twilio send you the link for the item as soon as it is in stock and then
+# Apple Pay is used to checkout.
+
+# INSTRUCTIONS #
+# First locate the line at the end of the script that says "browser = webdriver.Chrome(r'....').
+# Replace the directory with the directory the chrome webdriver you have downloaded.
+# Next, I recommend performing a test run of the script to ensure everything is functioning properly before continuing.
+# After you have verified proper functionality, inspect the element on the webpage for the product you want and
+# change the 5 digit number following 'grouped-product-item-xxxxx'. You can add multiple items as long as they
+# are on the same page. Then change the URL to that of your desired product.
+# You should run one last test to ensure functionality before entering personal information.
+# Make sure to enter in all of the REQUIRED information below.
 
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
@@ -17,10 +28,10 @@ import os
 
 # Info from Rogue Website
 class WebpageInfo:
-    #product = ('grouped-product-item-75745', 'grouped-product-item-75739', 'grouped-product-item-75741')    # REQUIRED
-    #URL = 'https://www.roguefitness.com/rogue-add-on-change-plate-pair'                                     # REQUIRED
-    URL = 'https://www.roguefitness.com/rogue-fleck-plates'
-    product = ('grouped-product-item-85751', 'grouped-product-item-85749', 'grouped-product-item-85745', 'grouped-product-item-85743', 'grouped-product-item-85741')
+    product = ('grouped-product-item-75745', 'grouped-product-item-75739', 'grouped-product-item-75741')    # REQUIRED
+    URL = 'https://www.roguefitness.com/rogue-add-on-change-plate-pair'                                     # REQUIRED
+    #URL = 'https://www.roguefitness.com/rogue-fleck-plates'
+    #product = ('grouped-product-item-85751', 'grouped-product-item-85749', 'grouped-product-item-85745', 'grouped-product-item-85743', 'grouped-product-item-85741')
 
 
 # Personal Checkout Info
@@ -38,12 +49,13 @@ class PersonalInfo:
     exp_year = '2021'                           # REQUIRED
     cvv = '999'                                 # REQUIRED
 
-# If you have a Twilio Account and want to opt-in for text messages when your
-# item is in stock and after the item is purchased, fill in the required information below.
+# A twilio account is REQUIRED to opt-in for text message updates when your
+# item is in stock. To enable, fill in the required information below with your twilio account ID, twilio auth token,
+# twilio phone number, and the number where you want to receive the notifications.
 
 
 def twilio_stock_alert():
-    optin = False       # Change this variable to get text updates with twilio only after specifying the paths.
+    optin = False       # Change optin to True to get text updates with twilio.
     if optin is True:
         account_sid = 'TWILIO_ACCOUNT_ID'  # REQUIRED FOR TEXT ALERTS
         auth_token = 'TWILIO_AUTH_TOKEN'     # REQUIRED FOR TEXT ALERTS
@@ -57,7 +69,7 @@ def twilio_stock_alert():
 
 
 def twilio_purchase_alert():
-    optin = False       # Change this variable to get text updates with twilio only after specifying the paths.
+    optin = False       # Change optin to True to get text updates with twilio.
     if optin is True:
         account_sid = 'TWILIO_ACCOUNT_ID'      # REQUIRED FOR TEXT ALERTS
         auth_token = 'TWILIO_AUTH_TOKEN'         # REQUIRED FOR TEXT ALERTS
@@ -65,13 +77,11 @@ def twilio_purchase_alert():
         client = Client(account_sid, auth_token)
         client.messages.create(from_='TWILIO_PHONE_NUMBER',            # REQUIRED FOR TEXT ALERTS
                                to='CELL_PHONE_NUMBER',               # REQUIRED FOR TEXT ALERTS
-                               body='The Rogue Automated Checkout Bot has successfully placed your order.\n'
-                                    ' Thank you for using and please consider donating.'
+                               body='The Rogue Automated Checkout Bot has successfully completed.\n'
+                                    'Please check email to verify purchase, thank you for using.\n'
+                                    ''
                                )
     return 0
-# Second you must specify the location of your browser executable.
-# We will make the webdriver location a variable "browser"
-# Make sure you have the webdriver for your browser downloaded.
 
 
 def webpage_status(browser):
@@ -83,7 +93,7 @@ def webpage_status(browser):
             try:
                 element = browser.find_element_by_id(x)
                 # availability = browser.find_element_by_class_name('availability')
-                if element.is_displayed() is True: #and availability.is_displayed() is False:
+                if element.is_displayed() is True: # and availability.is_displayed() is False:
                     update = 1
                     break
             except:
@@ -91,13 +101,13 @@ def webpage_status(browser):
         if update is 1:
             break
         browser.refresh()
-        time.sleep(20)          #This is the page refresh frequency in seconds. Don't refresh too often or you risk an IP ban.
+        time.sleep(20)   # This is the page refresh frequency in seconds. Don't refresh too often or you risk an IP ban.
     print("One or more target products in stock, proceeding to checkout.\n")
     twilio_stock_alert()
     return browser
 
 
-def rogue_checkout(browser):
+def rogue_checkout_plates(browser):
     done = 0
     for i in WebpageInfo.product:
         try:
@@ -109,34 +119,25 @@ def rogue_checkout(browser):
             print("item", i, "was not in stock")
             pass        # Will move on to the next item if one of the items is not in stock
 
-# Now you will need to checkout by clicking add to cart. We will make the "add to cart" button an element.
+    add = browser.find_element_by_class_name('add-to-box')          # Find add to cart element on page.
+    add.click()                                                     # Click add to cart.
 
-    add = browser.find_element_by_class_name('add-to-box')
-
-# Now click the add element that was created.
-
-    add.click()
-
-# The checkout page should appear, now we must click checkout.
+# The checkout page should appear
 #     if WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'#side-cart > div > div.cart-messages-container > div > div'))):
 #         return done is False        # Checks if there is an error message when adding items to cart.
 
+    # Finds checkout element on the side cart.
     checkout = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, '#side-cart > div > div.off-canvas-footer > div:nth-child(1) > div.v-col-8 > button')))
-    # checkout = browser.find_element_by_class_name('v-btn-checkout-button')
-    # time.sleep(.5)      Optimized to avoid waiting by using wait until element clickable
-    checkout.click()
+    checkout.click()        # Clicks checkout element.
 
-# This should bring you to the checkout method screen. We will pick checkout as guest.
-
+    # Find checkout as guest element and click.
     checkout_guest = browser.find_element_by_xpath("//button[text()= 'Checkout as Guest']")
     checkout_guest.click()
 
-    # This should bring you to the shipping information page and autofill information you defined at beginning.
+    # Autofill information defined in Personal Info.
 
-    # time.sleep(5)
     first_name_field = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.NAME, 'firstname')))
-    # first_name_field = browser.find_element_by_name('firstname')
     first_name_field.send_keys(PersonalInfo.first_name)
 
     last_name_field = browser.find_element_by_name('lastname')
@@ -160,41 +161,41 @@ def rogue_checkout(browser):
 
     email_field = browser.find_element_by_id('checkout:email')
     email_field.send_keys(PersonalInfo.email)
-    time.sleep(1)
 
+    # Find and click the view shipping options button.
     view_shipping_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
         (By.XPATH, '//button[text()= "View Shipping Options"]')))
-    # view_shipping_button = browser.find_element_by_xpath('//button[text()= "View Shipping Options"]')
     view_shipping_button.click()
-    # time.sleep(7)
 
+    # Find and click the confirm shipping button.
     confirm_ship_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
         (By.XPATH, '//button[text()= "Continue with this address"]')))
-    # confirm_ship_button = browser.find_element_by_xpath("//button[text()= 'Continue with this address']")
     confirm_ship_button.click()
 
+    # Find and click the continue to payment button.
     continue_to_payment_button = browser.find_element_by_xpath("//button[text()= 'Continue to Payment']")
     continue_to_payment_button.click()
-    # time.sleep(5)
 
+    # Changes iframe to that of credit card number element location.
     curr_frame = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
         (By.XPATH, '//iframe[@src="https://core.spreedly.com/v1/embedded/number-frame.html?v=1.50"]')))
-    # curr_frame = browser.find_element_by_xpath(
-    #     '//iframe[@src="https://core.spreedly.com/v1/embedded/number-frame.html?v=1.49"]')
-    browser.switch_to.frame(curr_frame)
 
+    # Selects and fills the card number section from the Personal Info.
+    browser.switch_to.frame(curr_frame)
     card_number_field = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'card_number')))
-    #card_number_field = browser.find_element_by_id('card_number')
     card_number_field.send_keys(PersonalInfo.card_number)
 
+    # Switches browser out of the credit card iframe. Needs to be done to locate the iframe for the cvv.
     browser.switch_to.default_content()
 
+    # Switches to the iframe for the cvv then fills with information from Personal Info.
     curr_frame = browser.find_element_by_xpath(
         '//iframe[@src="https://core.spreedly.com/v1/embedded/cvv-frame.html?v=1.50"]')
     browser.switch_to.frame(curr_frame)
     cvv_field = browser.find_element_by_id('cvv')
     cvv_field.send_keys(PersonalInfo.cvv)
 
+    # Switches back out of the cvv iframe to the default body.
     browser.switch_to.default_content()
 
     expire_month_field = browser.find_element_by_id('expirationmonth')
@@ -203,7 +204,6 @@ def rogue_checkout(browser):
     expire_year_field = browser.find_element_by_id('expirationyear')
     expire_year_field.send_keys(PersonalInfo.exp_year)
 
-    # time.sleep(.5)
     review_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
         (By.XPATH, '//button[text()= "Continue to Review"]')))
     # review_button = browser.find_element_by_xpath('//button[text()= "Continue to Review"]')
@@ -224,9 +224,10 @@ def rogue_checkout(browser):
         print("\nThe bot successfully placed an order, check your email to verify.\n"
               "Thank you for using my Rogue Automated Checkout Bot, hope it helped you, happy lifting!")
 
+
 if __name__ == '__main__':
     browser = webdriver.Chrome(r'C:\Users\damie\Documents\GitHub\Rogue_automated_checkout_bot\chromedriver')
     # while done is False:
     webpage_status(browser)
-    rogue_checkout(browser)
+    rogue_checkout_plates(browser)
 
