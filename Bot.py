a9a13857fce041b69a0c9ef1a07f8e89c5455772
damienlevitt <damien.levitt@sjsu.edu>
2020-07-28 22:33:32 -0700

    added functions for picture product selection when adding to cart.

--- Bot.py
+++ Bot.py
@@ -21,6 +21,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 from twilio.rest import Client
 import time
 import os
@@ -28,10 +29,10 @@ import os
 
 # Info from Rogue Website
 class WebpageInfo:
-    product = ('grouped-product-item-75745', 'grouped-product-item-75739', 'grouped-product-item-75741')    # REQUIRED
-    URL = 'https://www.roguefitness.com/rogue-add-on-change-plate-pair'                                     # REQUIRED
-    #URL = 'https://www.roguefitness.com/rogue-fleck-plates'
-    #product = ('grouped-product-item-85751', 'grouped-product-item-85749', 'grouped-product-item-85745', 'grouped-product-item-85743', 'grouped-product-item-85741')
+    #product = ('grouped-product-item-75745', 'grouped-product-item-75739', 'grouped-product-item-75741')    # REQUIRED
+    #URL = 'https://www.roguefitness.com/rogue-add-on-change-plate-pair'                                     # REQUIRED
+    URL = 'https://www.roguefitness.com/rogue-fleck-plates'
+    product = ('grouped-product-item-85751', 'grouped-product-item-85749','grouped-product-item-85747', 'grouped-product-item-85745', 'grouped-product-item-85743', 'grouped-product-item-85741')
 
 
 # Personal Checkout Info
@@ -84,6 +85,32 @@ def twilio_purchase_alert():
     return 0
 
 
+def reddit_restock_post():
+    thread = 'https://www.reddit.com/r/homegym/comments/hx101e/stock_and_shipping_thread_24_july_2020/'
+    chrome_options = Options()
+    chrome_options.add_experimental_option("prefs", {'profile.default_content_setting_values.notifications': 2})
+    driver2 = webdriver.Chrome(chrome_options=chrome_options)
+    driver2.get(thread)
+    login = driver2.find_element_by_css_selector(
+        '#SHORTCUT_FOCUSABLE_DIV > div:nth-child(4) > div > div._1npCwF50X2J7Wt82SZi6J0._3OGqXkiUb_0ZMlksb26boO > div.u35lf2ynn4jHsVUwPmNU.Dx3UxiK86VcfkFQVHNXNi._3KaECfUAGLfWQPO5eNjMNl > div.uI_hDmU5GSiudtABRz_37 > div._2GTMVdV2t3ka_zfkVHHo95 > a._1HunhFR-0b-AYs0WG9mU_P._3fM1M9rFBqKwfG-KJLnxPY._2nelDm85zKKmuD94NequP0')
+    login.click()
+    time.sleep(.2)
+    login_frame = WebDriverWait(driver2, 10).until(EC.element_to_be_clickable(
+        (By.XPATH, '//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[3]/div[2]/div/iframe')))
+    driver2.switch_to.frame(login_frame)
+    username = driver2.find_element_by_css_selector('#loginUsername')
+    username.send_keys('USERNAME')
+    password = driver2.find_element_by_css_selector('#loginPassword')
+    password.send_keys('PASSWORD')
+    login_button = driver2.find_element_by_css_selector('body > div > div > div.PageColumn.PageColumn__right > div > form > div.Onboarding__step.narrow > fieldset:nth-child(17) > button')
+    login_button.click()
+    driver2.switch_to.default_content()
+    textbox = WebDriverWait(driver2, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#SHORTCUT_FOCUSABLE_DIV > div:nth-child(4) > div > div > div > div._3ozFtOe6WpJEMUtxDOIvtU > div._1vyLCp-v-tE5QvZovwrASa > div._1OVBBWLtHoSPfGCRaPzpTf._3nSp9cdBpqL13CqjdMr2L_._2udhMC-jldHp_EpAuBeSR1.PaJBYLqPf_Gie2aZntVQ7 > div.uI_hDmU5GSiudtABRz_37 > div._1r4smTyOEZFO91uFIdWW6T.aUM8DQ_Nz5wL0EJc_wte6 > div:nth-child(2) > div > div > div._2baJGEALPiEMZpWB2iWQs7 > div > div:nth-child(1) > div > div > div')))
+    textbox.send_keys('Flecks in stock, go. https://www.roguefitness.com/rogue-fleck-plates')
+    comment_button = driver2.find_element_by_css_selector('#SHORTCUT_FOCUSABLE_DIV > div:nth-child(4) > div > div > div > div._3ozFtOe6WpJEMUtxDOIvtU > div._1vyLCp-v-tE5QvZovwrASa > div._1OVBBWLtHoSPfGCRaPzpTf._3nSp9cdBpqL13CqjdMr2L_._2udhMC-jldHp_EpAuBeSR1.PaJBYLqPf_Gie2aZntVQ7 > div.uI_hDmU5GSiudtABRz_37 > div._1r4smTyOEZFO91uFIdWW6T.aUM8DQ_Nz5wL0EJc_wte6 > div:nth-child(2) > div > div > div._17TqawK-44tH0psnHPIhzS.RQTXfVRnnTF5ont3w58rx > div._3SNMf5ZJL_5F1qxcZkD0Cp > button')
+    comment_button.click()
+
+
 def webpage_status(browser):
     browser.get(WebpageInfo.URL)
     update = 0
@@ -123,7 +150,12 @@ def rogue_checkout_plates(browser):
 
     add = browser.find_element_by_class_name('add-to-box')          # Find add to cart element on page.
     add.click()                                                     # Click add to cart.
+    main_checkout(browser)
 
+def rouge_checkout_box_select(browser):
+
+
+def main_checkout(browser):
 # The checkout page should appear
 #     if WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'#side-cart > div > div.cart-messages-container > div > div'))):
 #         return done is False        # Checks if there is an error message when adding items to cart.
@@ -183,7 +215,7 @@ def rogue_checkout_plates(browser):
 
     # Changes iframe to that of credit card number element location.
     curr_frame = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-        (By.XPATH, '//iframe[@src="https://core.spreedly.com/v1/embedded/number-frame.html?v=1.50"]')))
+        (By.XPATH, '//iframe[@src="https://core.spreedly.com/v1/embedded/number-frame.html?v=1.51"]')))
 
     # Selects and fills the card number section from the Personal Info.
     browser.switch_to.frame(curr_frame)
@@ -195,7 +227,7 @@ def rogue_checkout_plates(browser):
 
     # Switches to the iframe for the cvv then fills with information from Personal Info.
     curr_frame = browser.find_element_by_xpath(
-        '//iframe[@src="https://core.spreedly.com/v1/embedded/cvv-frame.html?v=1.50"]')
+        '//iframe[@src="https://core.spreedly.com/v1/embedded/cvv-frame.html?v=1.51"]')
     browser.switch_to.frame(curr_frame)
     cvv_field = browser.find_element_by_id('cvv')
     cvv_field.send_keys(PersonalInfo.cvv)
@@ -232,7 +264,7 @@ def rogue_checkout_plates(browser):
 
 if __name__ == '__main__':
     driver = webdriver.Chrome(r'C:\Users\damie\Documents\GitHub\Rogue_automated_checkout_bot\chromedriver')
-    # while done is False:
     webpage_status(driver)
-    rogue_checkout_plates(driver)
+    #rogue_checkout_plates(driver)
+    reddit_restock_post()
 
